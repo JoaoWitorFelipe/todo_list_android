2d6d2461b063fc15db3bae0138e0a26683dd8c57
JoaoWitorFelipe <joaowitorfelipe2@gmail.com>
2020-11-19 21:42:36 -0300

    change delete item logic to out class view holder

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
-
+    private val myDataSet: ArrayList<String> = arrayListOf("Item 1", "Item 2", "Item 3");
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         viewManager = LinearLayoutManager(this);
 
-        val myDataSet: ArrayList<String> = arrayListOf("Item 1", "Item 2", "Item 3");
-        viewAdapter = MyAdapter(myDataSet)
+        viewAdapter = MyAdapter(myDataSet, deleteItemFromList = { deleteItemFromList(it) } )
 
         recyclerView = findViewById<RecyclerView>(R.id.my_recycler_view).apply {
             setHasFixedSize(true)
@@ -49,8 +48,13 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun deleteItemFromList(position: Int) {
+        println(position)
+        myDataSet.removeAt(position)
+        viewAdapter.notifyItemRemoved(position)
+    }
 
-    class MyAdapter(private var myDataSet: ArrayList<String>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+    class MyAdapter(private var myDataSet: ArrayList<String>, private var deleteItemFromList: (position: Int) -> Unit) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
         class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -67,10 +71,7 @@ class MainActivity : AppCompatActivity() {
         override fun getItemCount(): Int = myDataSet.size
 
 
-        private fun deleteItemFromList(position: Int) {
-            myDataSet.removeAt(position)
-            notifyItemRemoved(position)
-        }
+
 
     }
 
